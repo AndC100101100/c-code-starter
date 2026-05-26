c1aa9a5103999c74b42894a024facfbcf47a4321
Andres <andrescastrogonzal245@outlook.com>
2026-05-26 00:32:52 -0500

    7.5.7, numbers

---
+++ 7_5_7-numbers/floating_nums.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	// 32 bit single precision floating numbers that allow for 6 decimal digits of precision
+	float num1 = 12.123456;
+	printf("Example 1: %f\n", num1);
+	
+	float num2 = 123456.123456;
+	printf("Example 2: %f\n", num2);
+	
+	float num3 = 123.123456;
+	printf("Example 3: %f\n", num3);
+	
+	float num4 = 12.1234567;
+	printf("Example 4: %f\n", num4);
+}
