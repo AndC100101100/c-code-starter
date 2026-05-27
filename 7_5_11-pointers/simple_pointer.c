45a624e0166de4e03fa7128179c078ae56f14aa5
Andres <andrescastrogonzal245@outlook.com>
2026-05-27 17:23:04 -0500

    7.5.11, Pointers, Pointer Arithmetic

--- 7_5_11-pointers/simple_pointer.c
+++ 7_5_11-pointers/simple_pointer.c
@@ -2,21 +2,40 @@
 
 int main(){
 
-	/*this is a very verbose version of the code
-	 * it can be writen like:
-	 * char greetings[]= "hello";
-	 * char *msg_ptr = greeting*/
+
 	char greeting[] = "Hello";
-	char *msg_ptr; // our pointer
-	
-	msg_ptr = &greeting[0]; //this sets the value for the pointer to point to the addressof the greeting var, were & means the address of the item
-
-
+	char *msg_ptr = &greeting[0]; ; // our pointer, this sets the value for the pointer to point to the addressof the greeting var, were & means the address of the item
 
 	printf("Address of greeting:\n%p\n\n", &greeting);
 	printf("Address stored in msg_ptr:\n%p\n\n", msg_ptr);
 	printf("Character at *msg_ptr: %c\n\n", *msg_ptr);
+	printf("Address of *msg_ptr: %p\n\n", &msg_ptr);// our message pointer has an address, which contents are that value of the greetings memory address
 
+ 	// H, e, l, l, o, \o
+   	printf("Address: %p, Value: %d, Character: %c\n", msg_ptr, *msg_ptr, *msg_ptr);
+ 	msg_ptr++;
+ 	printf("Address: %p, Value: %d, Character: %c\n", msg_ptr, *msg_ptr, *msg_ptr);
+ 	msg_ptr++;
+ 	printf("Address: %p, Value: %d, Character: %c\n", msg_ptr, *msg_ptr, *msg_ptr);
+ 	msg_ptr++;
+ 	printf("Address: %p, Value: %d, Character: %c\n", msg_ptr, *msg_ptr, *msg_ptr);
+ 	msg_ptr++;
+ 	printf("Address: %p, Value: %d, Character: %c\n", msg_ptr, *msg_ptr, *msg_ptr);
+ 	msg_ptr++;
+ 	printf("Address: %p, Value: %d, Character: %c\n\n", msg_ptr, *msg_ptr, *msg_ptr);
+   	
+	// Adjacent data!
+  	msg_ptr++;
+   	printf("Address: %p, Value: %d, Character: %c\n", msg_ptr, *msg_ptr, *msg_ptr);
+   	msg_ptr++;
+   	printf("Address: %p, Value: %d, Character: %c\n", msg_ptr, *msg_ptr, *msg_ptr);
+   	msg_ptr++;
+   	printf("Address: %p, Value: %d, Character: %c\n", msg_ptr, *msg_ptr, *msg_ptr);
+   	msg_ptr++;
+   	printf("Address: %p, Value: %d, Character: %c\n", msg_ptr, *msg_ptr, *msg_ptr);
+   	msg_ptr++;
+   	printf("Address: %p, Value: %d, Character: %c\n", msg_ptr, *msg_ptr, *msg_ptr);
+   	msg_ptr++;
 	return 0;
 
 
