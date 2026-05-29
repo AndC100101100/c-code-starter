b9368d68b9c8a12043bdd44bc123d207c4f1fcea
Andres <andrescastrogonzal245@outlook.com>
2026-05-29 02:47:04 -0500

    7.6.2, Loops, Loading bar finished or somewhat

--- 7_6/7_6_2-loops/loading-bar.c
+++ 7_6/7_6_2-loops/loading-bar.c
@@ -3,12 +3,14 @@
 int main(){
 	char *block = "█";
     	char *period = ".";
-	int loaded = 7;
-	for (int i = 1; i <=10; i++){
-		printf("%s", i <= loaded ? block : period);
+	int loaded = 0;
 	
-	}
-
-
+	for (int row = 1; row <= 10; row++) {
+        	loaded++;
+        	for (int i = 1; i <= 10; i++) {
+            		printf("%s", i <= loaded ? block : period);
+        	}
+        	printf(" %d%%\n", loaded * 10);
+    	}
 	return 0;
 }
