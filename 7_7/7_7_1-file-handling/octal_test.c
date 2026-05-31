49ce9511024e151e4c25c6e94d27db784df86644
Andres <andrescastrogonzal245@outlook.com>
2026-05-30 19:38:18 -0500

    7.7.1, file handling, testing reading octal encoded, file decoded

--- 7_7/7_7_1-file-handling/octal_test.c
+++ 7_7/7_7_1-file-handling/octal_test.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
-int main() {
+int main(){
 	FILE *fp = fopen("file.txt", "r");
 	char buff[2];
+	// using ftell to inform us of the pointer position
+	printf("File pointer at byte %ld\n", ftell(fp));
+	fseek(fp, 44, SEEK_SET); // move the pointer 50 bytes from the start of the file
+	printf("File pointer at byte %ld\n", ftell(fp));
 
 
-
-	while (fscanf(fp, "%hho", buff) != EOF){
+	for (int i = 0; i < 9; i++){
+		fscanf(fp, "%hho", buff);
 		printf("%s", buff);
 	}
 
-    fclose(fp);
-    return 0;
+    	printf("\nFile pointer at byte %ld\n", ftell(fp));
+    	rewind(fp);
+    	printf("File pointer at byte %ld\n", ftell(fp));
+    	fclose(fp);
+	
+	return 0;
 }
-
